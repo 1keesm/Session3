cb7a7de1f59a612ad3e4d94f0591c62d1fbf8e04
1keesm <kees0meijers@gmail.com>
2025-01-15 18:52:23 +0100

    Added arithmetic and boolean

--- bool_operators.py
+++ bool_operators.py
@@ -4,3 +4,8 @@ print(not 7)# false because 7 is by default true
 
 print(True or False)
 print(True and False)
+print(2 or 3 or 4)
+print(False or 8 or 4 or True) # for or its the first true you encounter or last false if all false
+print(1 and 2 and 3) # for and its the last true you encounter, or the first true
+print(1 and 0.0 and 4)
+print(False or 0 or None)# last false you encounter
