bb91cbd3e672032efdc15996dcddf958ac579b61
1keesm <kees0meijers@gmail.com>
2025-01-15 18:47:52 +0100

    Arithmetic operators

---
+++ arithmetic_operator.py
@@ -0,0 +1,11 @@
+print(2+6)
+print(9-99)
+print(3*8)
+print(20/5) # always produces float result
+print(22/5)
+print(20//5) # integer division
+print(22%5)
+print(10%6)
+print(2**3)
+
+# everything by default is considered true except for none, 0, 0.o, and empty sequences: ",[],()
